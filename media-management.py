965e23012f09993026bae1e3f1a86200c06bf929
Rick Meels <rick.meels@gmail.com>
2022-11-21 21:30:25 +0100

    Media + added 'get_tautulli_library_media_info' * modified attributes for 'parse_tautulli_libraries_table'

--- media-management.py
+++ media-management.py
@@ -181,6 +181,7 @@ def parse_tautulli_libraries_table(payload):
         'section_id': payload['section_id'],
         'rating_key': payload['rating_key'],
         'section_type': payload['section_type'],
+        'count': payload['count'],
     }
 
     if result['section_type'] == 'live':
@@ -189,6 +190,43 @@ def parse_tautulli_libraries_table(payload):
     return result
 
 
+def get_tautulli_library_media_info(tautulli_library):
+    logging.info('📦 Retrieving Tautulli library (\033[1m{}\033[0m) media info'.format(tautulli_library['section_name']))
+
+    payload = {
+        'apikey': TAUTULLI_APIKEY,
+        'cmd': 'get_library_media_info',
+        'section_id': tautulli_library['section_id'],
+        'order_column': 'last_played',
+        'order_dir': 'desc',
+        'length': 100,
+        'start': 0
+    }
+
+    res_data = []
+
+    try:
+        while payload['start'] < tautulli_library['count']:
+            r = requests.get(TAUTULLI_URL.rstrip('/') + '/api/v2', params=payload)
+            response = r.json()
+
+            logging.debug('get_tautulli_library_media_info response: ' + str(res_data))
+
+            if len(response['response']['data']['data']) == 0:
+                logging.warning('🚧 No Tautulli library (\033[1m{}\033[0m) media found'.format(tautulli_library['section_name']))
+            else:
+                for media in response['response']['data']['data']:
+                    res_data.append(media)
+            payload['start'] = payload['start'] + 100
+
+        logging.info('✅ Retrieved (\033[1m{}\033[0m) Tautulli library (\033[1m{}\033[0m) media info'.format(len(res_data), tautulli_library['section_name']))
+        return res_data
+    except Exception as e:
+        logging.error("❌ Tautulli API 'get_tautulli_library_media_info' request failed: {0}".format(e))
+
+
+
+# Plex logic
 parsed_plex_libraries = []
 plex_libraries = get_plex_libraries()
 
@@ -200,8 +238,13 @@ for library in plex_libraries:
 logging.debug('parse_plex_library_result response: ' + str(parsed_plex_libraries))
 logging.info("✅ Parsed (\033[1m{}\033[0m) 'get_plex_libraries' result".format(len(plex_libraries)))
 
+# Radarr logic
 radarr_movie_list = get_radarr_movies()
+
+# Sonarr logic
 sonarr_series_list = get_sonarr_series()
+
+# Tautulli logic
 tautulli_libraries_table = get_tautulli_libraries_table()
 
 parsed_tautulli_libraries_table = []
@@ -213,11 +256,14 @@ for library in tautulli_libraries_table:
     if result is not None:
         parsed_tautulli_libraries_table.append(result)
 
+
 logging.debug('parse_tautulli_libraries_table response: ' + str(parsed_tautulli_libraries_table))
 logging.info("✅ Parsed (\033[1m{}\033[0m) 'get_tautulli_libraries_table' result".format(len(parsed_tautulli_libraries_table)))
 
+tautulli_library_media_info = []
+
+for library in parsed_tautulli_libraries_table:
+    tautulli_library_media_info.append(get_tautulli_library_media_info(library))
+
+# Overseerr logic
 overseerr_media_list = get_overseerr_requests()
-
-
-
-
